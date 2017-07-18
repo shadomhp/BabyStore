13600795752037f3c0cabcf66f57af7c2a29d3a0
Fabio Zanardo <shadomhp@gmail.com>
2017-07-18 00:50:34 -0300

    Create Project and create Category and Product models

---
+++ BabyStore/DAL/StoreContext.cs
@@ -0,0 +1,15 @@
+﻿using BabyStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BabyStore.DAL
+{
+    public class StoreContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
