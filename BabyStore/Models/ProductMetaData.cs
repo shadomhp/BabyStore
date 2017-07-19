f3c3703cc30271528c95586451fde6c4b85751d9
Fabio Zanardo <shadomhp@gmail.com>
2017-07-18 22:04:22 -0300

    Add ProductMetaData

---
+++ BabyStore/Models/ProductMetaData.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BabyStore.Models
+{
+    [MetadataType(typeof(ProductMetaData))]
+    public partial class Product
+    {
+
+    }
+
+
+    public class ProductMetaData
+    {
+        [Display(Name = "Product Name")]
+        public string Name;
+    }
+}
