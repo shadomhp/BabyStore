b068817976f16110abddda3c465318d3d2991a3f
Fabio Zanardo <shadomhp@gmail.com>
2017-07-27 12:50:17 -0300

    Adding a Roles View Model and RolesAdminController

---
+++ BabyStore/ViewModels/AdminViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BabyStore.ViewModels
+{
+    public class RoleViewModel
+    {
+        public string Id { get; set;}
+        [Required(AllowEmptyStrings = false)]
+        [Display(Name ="Role Nome")]
+        public string Name { get; set; }
+    }
+}
