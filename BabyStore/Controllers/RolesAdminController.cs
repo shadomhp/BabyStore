a86b6042f2aa5f6c9c1f0d75b59dc6c507e23dfe
Fabio Zanardo <shadomhp@gmail.com>
2017-07-28 00:23:31 -0300

    Editing a Role

--- BabyStore/Controllers/RolesAdminController.cs
+++ BabyStore/Controllers/RolesAdminController.cs
@@ -107,25 +107,34 @@ namespace BabyStore.Controllers
         }
 
         // GET: RolesAdmin/Edit/5
-        public ActionResult Edit(int id)
+        public async Task<ActionResult> Edit(string id)
         {
-            return View();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var role = await RoleManager.FindByIdAsync(id);
+            if (role == null)
+            {
+                return HttpNotFound();
+            }
+            RoleViewModel roleModel = new RoleViewModel { Id = role.Id, Name = role.Name };
+
+            return View(roleModel);
         }
 
         // POST: RolesAdmin/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public async Task<ActionResult> Edit(RoleViewModel roleModel)
         {
-            try
+            if (ModelState.IsValid)
             {
-                // TODO: Add update logic here
-
+                var role = await RoleManager.FindByIdAsync(roleModel.Id);
+                role.Name = roleModel.Name;
+                await RoleManager.UpdateAsync(role);
                 return RedirectToAction("Index");
             }
-            catch
-            {
-                return View();
-            }
+            return View();
         }
 
         // GET: RolesAdmin/Delete/5
