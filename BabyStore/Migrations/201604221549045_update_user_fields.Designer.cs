41a1f428adab16139ae5736979c93d34b0b131c4
Fabio Zanardo <shadomhp@gmail.com>
2017-07-29 02:52:43 -0300

    Migração e Copia da base e migrations do projeto do livro

--- BabyStore/Migrations/201604221549045_update_user_fields.Designer.cs
+++ BabyStore/Migrations/201604221549045_update_user_fields.Designer.cs
@@ -7,13 +7,13 @@ namespace BabyStore.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class CategoryNameValidation : IMigrationMetadata
+    public sealed partial class update_user_fields : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(CategoryNameValidation));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(update_user_fields));
         
         string IMigrationMetadata.Id
         {
-            get { return "201707210322115_CategoryNameValidation"; }
+            get { return "201604221549045_update_user_fields"; }
         }
         
         string IMigrationMetadata.Source
